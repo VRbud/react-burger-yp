719b003a3a804e4081517c95e71b39403638f16a
Vladislav <ftagreemaster@gmail.com>
2023-05-04 00:24:25 -0400

    убирает лишние запросы

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,28 +1,10 @@
-import { useDispatch, useSelector } from "react-redux";
-import LoginFrom from "../components/LoginForm/LoginFrom";
-
-import { useCallback, useEffect } from "react";
-import { checkLogin } from "../services/actions/auth";
+import LoginForm from "../components/LoginForm/LoginForm";
 
 function LoginPage() {
-  const { loginData } = useSelector((state) => state.auth);
-
-  const dispatch = useDispatch();
-
-  const init = useCallback(async () => {
-    if (!loginData) {
-      await dispatch(checkLogin());
-    }
-  }, [dispatch, loginData]);
-
-  useEffect(() => {
-    init();
-  }, [init, dispatch]);
-
   return (
     <>
       <main>
-        <LoginFrom type="login" />
+        <LoginForm />
       </main>
     </>
   );
